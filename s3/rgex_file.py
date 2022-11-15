7d12df27aba96af37f2bbe046f85d64d9b341ff9
Javagal Aswin Venkataramanan <Javagal.Aswin@valuelabs.com>
2022-11-15 19:30:49 +0530

    changing regex script

--- s3/rgex_file.py
+++ s3/rgex_file.py
@@ -1,19 +1,25 @@
-lookup = 'pulumi_aws'
+lookup = "import pulumi_aws as aws\n"
 
-with open('import.txt') as myFile:
-    for num, line in enumerate(myFile, 1):
-        if lookup in line:
-            with open(r"import.txt", 'r+') as fp:
-                # read a store all lines into list
-                lines = fp.readlines()
-                # move file pointer to the beginning of a file
-                fp.seek(0)
-                # truncate the file
-                fp.truncate()
-                #print(num)
-                # start writing lines except the comment line
-                # lines[1:] from line 2 to last line
-                fp.writelines(lines[num - 2:])
+
+file1 = open("import.txt",'r')
+file1_lines = file1.readlines()
+
+for i in range(len(file1_lines)):
+
+    if file1_lines[i] == lookup:
+        print('s')
+        num = i+1
+        with open(r"import.txt", 'r+') as fp:
+            # read a store all lines into list
+            lines = fp.readlines()
+            # move file pointer to the beginning of a file
+            fp.seek(0)
+            # truncate the file
+            fp.truncate()
+            # print(num)
+            # start writing lines except the comment line
+            # lines[1:] from line 2 to last line
+            fp.writelines(lines[num - 2:])
 
 
 with open("import.txt") as f:
