9b46a7f018f56c1b144b7ca79c717a3e54ac9915
Javagal Aswin Venkataramanan <Javagal.Aswin@valuelabs.com>
2022-11-15 17:27:17 +0530

    removing venv

--- s3/__main__.py
+++ s3/__main__.py
@@ -15,16 +15,8 @@ instance_1 = aws.ec2.Instance("Instance-1",
                               opts=ResourceOptions(import_='i-0a8a23d36b28d221d')
                               )
 
-# Creating new resource
-bucket = aws.s3.Bucket("bucket-1",
-                       acl="private",
-                       tags={
-                           "Environment": "Dev",
-                           "Name": "Pulumi-poc-bucket",
-                       })
-
 
 # Exporting resource outputs
-pulumi.export('bucket_name', bucket.id)
+
 pulumi.export('instance_id', instance_1.id)
 pulumi.export('instance_type', instance_1.instance_type)
